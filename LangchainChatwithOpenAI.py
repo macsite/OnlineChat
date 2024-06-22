4628ae7c6f68cfa0a62d95d97f274eac863eea47
macsite <97990160+macsite@users.noreply.github.com>
2024-06-22 22:24:25 +0000

    req2

--- LangchainChatwithOpenAI.py
+++ LangchainChatwithOpenAI.py
@@ -1,55 +1,55 @@
 
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+# from langchain_openai import ChatOpenAI
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
 
-import streamlit as st 
-import os 
+# import streamlit as st 
+# import os 
 
-os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-# Langsmith tracking
-os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
-os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+# os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+# # Langsmith tracking
+# os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
+# os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
 
-# Prompt template
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", "You are a helpful assistant. PLease response to the querries"),
-        ("user","Question: {question}")
-    ]
-)
+# # Prompt template
+# prompt = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", "You are a helpful assistant. PLease response to the querries"),
+#         ("user","Question: {question}")
+#     ]
+# )
 
-# streamlit
-st.title("Langchain Chat with OpenAI")
-input_text = st.text_input("Search the topic you want.","...")
+# # streamlit
+# st.title("Langchain Chat with OpenAI")
+# input_text = st.text_input("Search the topic you want.","...")
 
-# OpenAI LLM
-llm = ChatOpenAI(model="gpt-3.5-turbo")
-output_parser = StrOutputParser()
-chain = prompt|llm|output_parser
+# # OpenAI LLM
+# llm = ChatOpenAI(model="gpt-3.5-turbo")
+# output_parser = StrOutputParser()
+# chain = prompt|llm|output_parser
 
-if input_text:
-    st.write(chain.invoke({'question':input_text}))
-
-
+# if input_text:
+#     st.write(chain.invoke({'question':input_text}))
 
 
 
-# import streamlit as st
-# from langchain.llms import OpenAI
 
-# st.title('🦜🔗 Quickstart App')
 
-# openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+import streamlit as st
+from langchain.llms import OpenAI
 
-# def generate_response(input_text):
-#     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-#     st.info(llm(input_text))
+st.title('🦜🔗 Quickstart App')
 
-# with st.form('my_form'):
-#     text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-#     submitted = st.form_submit_button('Submit')
-#     if not openai_api_key.startswith('sk-'):
-#         st.warning('Please enter your OpenAI API key!', icon='⚠')
-#     if submitted and openai_api_key.startswith('sk-'):
-#         generate_response(text)
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+
+def generate_response(input_text):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
+
+with st.form('my_form'):
+    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
