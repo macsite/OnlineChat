8af2f989631faec8f0a96abdb75f723660205544
macsite <97990160+macsite@users.noreply.github.com>
2024-06-22 21:58:08 +0000

    sfd

--- LangchainChatwithOpenAI.py
+++ LangchainChatwithOpenAI.py
@@ -1,10 +1,10 @@
 
-# from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+# # from langchain_openai import ChatOpenAI
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
 
-import streamlit as st 
-import os 
+# import streamlit as st 
+# import os 
 
 # os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 # # Langsmith tracking
@@ -19,9 +19,9 @@ import os
 #     ]
 # )
 
-# streamlit
-st.title("Langchain Chat with OpenAI")
-input_text = st.text_input("Search the topic you want.","...")
+# # streamlit
+# st.title("Langchain Chat with OpenAI")
+# input_text = st.text_input("Search the topic you want.","...")
 
 # # OpenAI LLM
 # llm = ChatOpenAI(model="gpt-3.5-turbo")
@@ -31,3 +31,25 @@ input_text = st.text_input("Search the topic you want.","...")
 # if input_text:
 #     st.write(chain.invoke({'question':input_text}))
 
+
+
+
+
+import streamlit as st
+from langchain.llms import OpenAI
+
+st.title('🦜🔗 Quickstart App')
+
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+
+def generate_response(input_text):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
+
+with st.form('my_form'):
+    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
