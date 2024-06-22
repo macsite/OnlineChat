49afe0ced925038b7ac0d60969f79cebef6180f7
macsite <97990160+macsite@users.noreply.github.com>
2024-06-22 21:55:46 +0000

    po staremu

--- LangchainChatwithOpenAI.py
+++ LangchainChatwithOpenAI.py
@@ -1,33 +1,33 @@
 
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 import streamlit as st 
 import os 
 
-os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-# Langsmith tracking
-os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
-os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+# os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+# # Langsmith tracking
+# os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
+# os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
 
-# Prompt template
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", "You are a helpful assistant. PLease response to the querries"),
-        ("user","Question: {question}")
-    ]
-)
+# # Prompt template
+# prompt = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", "You are a helpful assistant. PLease response to the querries"),
+#         ("user","Question: {question}")
+#     ]
+# )
 
 # streamlit
 st.title("Langchain Chat with OpenAI")
 input_text = st.text_input("Search the topic you want.","...")
 
-# OpenAI LLM
-llm = ChatOpenAI(model="gpt-3.5-turbo")
-output_parser = StrOutputParser()
-chain = prompt|llm|output_parser
+# # OpenAI LLM
+# llm = ChatOpenAI(model="gpt-3.5-turbo")
+# output_parser = StrOutputParser()
+# chain = prompt|llm|output_parser
 
-if input_text:
-    st.write(chain.invoke({'question':input_text}))
+# if input_text:
+#     st.write(chain.invoke({'question':input_text}))
 
