18a270958dcbbdbc3682cfca79370f2f7ee8a84c
macsite <97990160+macsite@users.noreply.github.com>
2024-06-23 15:25:44 +0000

    fraszka

--- LangchainChatwithOpenAI.py
+++ LangchainChatwithOpenAI.py
@@ -9,23 +9,26 @@ import os
 
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 # Langsmith tracking
-# os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
 os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
 
 # Prompt template
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a helpful assistant. PLease response to the querries"),
+        ("system", "Jesteś autorem bajek, wierszy i fraszek. Napisz śmieszną fraszkę o zwierzatku, na 4 wersy. Niech będzie zabawny, ale nie prześmiewczy. Weż pod uwagę dane o zwierzęciu podane poniżej."),
         ("user","Question: {question}")
     ]
 )
 
 # streamlit
 st.title("Langchain Chat with OpenAI")
-input_text = st.text_input("Search the topic you want.","...")
+input_text = st.text_input("Opisz swojego zwierzaka, a ja napiszę o nim wierszyk.","""imie: Bajtek
+gatunek: pies
+rasa: mieszaniec
+charakterystyka: szybki, kochany, przywiązany, oddany, lubi patyki i spacery, lubi mięsko i wino""")
 
 # OpenAI LLM
-llm = ChatOpenAI(model="gpt-3.5-turbo")
+# llm = ChatOpenAI(model="gpt-3.5-turbo")
+llm = ChatOpenAI(model="gpt-4o")
 output_parser = StrOutputParser()
 chain = prompt|llm|output_parser
 
